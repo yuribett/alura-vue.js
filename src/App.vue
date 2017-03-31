90e72a604d7984310d654769917d000ec1acc308
Yuri Bett <yuribett@gmail.com>
2017-03-31 06:46:37 -0300

    Adding panel style

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,16 @@
 <template>
-  <div>
-    <h1>{{ title }}</h1>
-    <ul v-for="photo of photos">
-      <li><img :src="photo.url" :alt="photo.title"></li>
+  <div class="container">
+    <h1 class="centralized">{{ title }}</h1>
+    
+    <ul class="list-photos">
+      <li class="list-photos-item" v-for="photo of photos">
+        <div class="panel">
+          <h2 class="panel-title">{{photo.title}}</h2>
+          <div class="panel-body">
+            <img class="img-responsive" :src="photo.url" :alt="photo.title">
+          </div><!-- fim panel-body -->
+        </div><!-- fim panel -->
+      </li>
     </ul>
     
   </div>
@@ -24,5 +32,46 @@ export default {
 </script>
 
 <style lang="scss">
+  .centralized {
+    text-align: center;
+  }
+
+  .container {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .list-photos {
+    list-style: none;
+  }
+
+  .list-photos .list-photos-item {
+    display: inline-block;
+  }
+
+  .img-responsive {
+    width: 95%;
+  }
+
+  .panel {
+    padding: 0 auto;
+    border: solid 2px grey;
+    display: inline-block;
+    margin: 5px;
+    box-shadow: 5px 5px 10px grey;
+    width: 200px;
+    height: 100%;
+    vertical-align: top;
+    text-align: center;
+  }
+
+  .panel .panel-title {
+    text-align: center;
+    background: lightblue;
+    margin: 0 0 15px 0;
+    padding: 10px;
+    text-transform: uppercase;
+  }
 
 </style>
